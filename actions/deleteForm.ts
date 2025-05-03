a81c4c9d047be8a91c8558198f81bdfe53c7009c
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-03 23:16:37 +0530

    The deleteForm Function This is an async function that takes a form ID as a parameter.
    It attempts to delete the form with the specified ID from the Form table using Prisma's delete() method.
    
    prisma.form.delete({ where: { id: formId } }):
    
    Deletes the form whose id matches formId.
    
    If the form doesn't exist, Prisma will throw an error, but in this code, it assumes the result might be null and checks for it.

--- actions/deleteForm.ts
+++ actions/deleteForm.ts
@@ -3,4 +3,16 @@
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 
+export const deleteForm = async (formId: number) => {
+  const form = await prisma.form.delete({
+    where: {
+      id: formId,
+    },
+  });
 
+  
+
+ 
+
+
+};
