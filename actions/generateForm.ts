ce9f9c3a5de487d2759070757a82d38f81e5cc63
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-03 23:20:17 +0530

    generateForm Function This function is triggered during a form submission where a user enters a description of the form they want to generate.

--- actions/generateForm.ts
+++ actions/generateForm.ts
@@ -6,8 +6,9 @@ import { z } from "zod";
 
 import { GoogleGenerativeAI } from "@google/generative-ai"; // Import Gemini
 import { revalidatePath } from "next/cache";
-
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" }); // Specify the right model
 
-
+export const generateForm = async (prevState: unknown, formData: FormData) => {
+  
+};
