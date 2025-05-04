274dd3c1ab2868baf3e7757c1611360f762d9397
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-04 16:23:53 +0530

    Imports Stripe: Imported to use Stripe types and functionality, especially for verifying webhook signatures.
    stripe: This is a pre-configured instance (usually initialized with your Stripe secret key in @/lib/stripe).
    
    createSubscription: A backend function that registers the user’s subscription in your database.
    
    NextResponse: Used to return structured HTTP responses in a Next.js server environment.

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,10 @@
+import Stripe from "stripe";
+import { stripe } from "@/lib/stripe";
+import { createSubscription } from "@/actions/userSubscription";
+import { NextResponse } from 'next/server';
+ 
+
+
+
+
+ 
