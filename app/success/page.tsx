ce572ea455e4f8315c9cab74b6a4f89bea11b392
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-04 19:11:53 +0530

    The SuccessPage Component This is a functional React component that displays a success message to the user after they have successfully upgraded their plan. The page informs the user that they can now create more forms and offers a button to navigate back to the homepage.

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -3,5 +3,8 @@ import { Button } from '@/components/ui/button'
 import { useRouter } from 'next/navigation'
 import React from 'react'
 
-
+const SuccessPage = () => {
+    const router = useRouter();
+  
+}
 
