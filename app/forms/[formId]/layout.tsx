f4c923cf55e5b2b91fc07acaffd84ad08599511f
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-04 19:07:03 +0530

    { children }: { children: React.ReactNode }:
    The children prop represents any nested content passed into this layout. This could be any page content or components that are wrapped by this layout.
    
    React.ReactNode is a type that allows any valid JSX content (strings, numbers, elements, etc.) to be passed as children.

--- app/forms/[formId]/layout.tsx
+++ app/forms/[formId]/layout.tsx
@@ -3,6 +3,8 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
- 
+  return (
+    
+  );
 };
 
