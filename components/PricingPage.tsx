898ff85735288fae99cdddf2692312f59f876fa6
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-04 18:32:16 +0530

    Imports React & useRouter: For building UI and navigation.
    Button, Card, CardContent, etc.: UI components used to structure and style the pricing cards.
    
    pricingPlan & PricingPlan: A predefined list of pricing plan objects with types from a local module.
    
    Badge, CheckCircle: Decorative and informative UI elements.
    
    Axios: Used to send HTTP requests (for Stripe session creation).
    
    "use client": Ensures this component runs only on the client side, as it uses hooks and browser-only logic.

---
+++ components/PricingPage.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { PricingPlan, pricingPlan } from "@/lib/pricingplan";
+import { Badge } from "./ui/badge";
+import { CheckCircle } from "lucide-react";
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+
+
+
+
