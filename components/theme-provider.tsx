c77ef490f3e7c590f862b70820cd123a2cf225e3
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-04 16:31:12 +0530

    Defining the ThemeProvider component The ThemeProvider is a wrapper component that takes in:
    children: the JSX elements/components it will wrap.
    
    ...props: any additional props (like attribute, defaultTheme, etc.) which are passed down to NextThemesProvider.
    
    The component uses TypeScript to strongly type its props:
    
    It extends React.ComponentProps<typeof NextThemesProvider>, meaning it accepts any props that NextThemesProvider accepts.

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -3,4 +3,9 @@
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
-
+ function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
