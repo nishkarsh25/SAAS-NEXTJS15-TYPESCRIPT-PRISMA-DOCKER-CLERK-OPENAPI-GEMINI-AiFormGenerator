e184f8f80bdf9ccbdccd2f85a50e969ed6834a72
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-04 18:58:10 +0530

    Component Definition – Edit This is an async server component because it fetches data from the server before rendering the page. The component is named Edit, implying that it’s used to edit a form.
    It receives params from the Next.js router (for dynamic routing via [formId]), and those are used to determine which form to edit.

--- app/dashboard/forms/edit/[formId]/page.tsx
+++ app/dashboard/forms/edit/[formId]/page.tsx
@@ -3,6 +3,13 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import prisma from "@/lib/prisma";
 import React from "react";
 
+const Edit = async ({ params }: { params: Promise<{ formId: string }> }) => {
+  const formId = (await params).formId;
 
+
+  
+
+  
+};
 
 
