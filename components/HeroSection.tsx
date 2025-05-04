5a3ea7db727924b2eb021f990a5459eaa919f454
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-04 16:56:22 +0530

    Props: The HeroSection component accepts the following props:
    totalForms: The number of forms the user has generated.
    
    isSubscribed: A boolean indicating whether the user has a subscription that allows them to generate more forms.

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -8,9 +8,25 @@ type SuggestionText = {
   text: string;
 };
 
+const suggestionBtnText: SuggestionText[] = [
+  {
+    label: "Job Application",
+    text: "Develop a basic job application form that serves as a one-page solution form collecting essential information from applicants.",
+  },
+  {
+    label: "Registration Form",
+    text: "Create a course registration form suitable form any scheool or instituition.",
+  },
+  {
+    label: "Feedback Form",
+    text: "Create a client feedback form to gather valuable insights from any clients.",
+  },
+];
 
-
-
+type Props = {
+  totalForms:number;
+  isSubscribed:boolean
+}
 
 
 
