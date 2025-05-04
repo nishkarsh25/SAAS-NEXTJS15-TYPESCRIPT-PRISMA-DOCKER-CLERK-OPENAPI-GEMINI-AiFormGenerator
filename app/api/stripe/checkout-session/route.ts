10535e36e9fbd9c28cade91f3daa183884113fee
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-04 14:45:13 +0530

    Handling the POST Request This function is triggered when the /api/... route receives a POST request. It's an async function, which means it can await promises (like JSON parsing or network calls).

--- app/api/stripe/checkout-session/route.ts
+++ app/api/stripe/checkout-session/route.ts
@@ -3,4 +3,16 @@ import { NextResponse } from "next/server";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
+export async function POST(req: Request) {
+  
 
+  
+
+  
+
+  
+
+  
+
+  
+}
