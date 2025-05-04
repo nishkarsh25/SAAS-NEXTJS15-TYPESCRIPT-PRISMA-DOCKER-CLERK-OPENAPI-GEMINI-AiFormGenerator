58d66c071dfef8a40d3e9143b54d35244cacdee0
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-04 18:53:29 +0530

    Component Definition: Submissions This is a server component for a dynamic route like /dashboard/forms/[formId]/submissions.
    Props:
    It accepts a params prop (a Promise) that eventually resolves to an object containing formId (a string representing the ID of the form whose submissions we want to fetch).

--- app/dashboard/forms/[formId]/submissions/page.tsx
+++ app/dashboard/forms/[formId]/submissions/page.tsx
@@ -2,6 +2,18 @@ import SubmissionsDetails from "@/components/SubmissionDetails";
 import prisma from "@/lib/prisma";
 import React from "react";
 
+const Submissions = async ({
+  params,
+}: {
+  params: Promise<{ formId: string }>;
+}) => {
+  
 
+  
+
+  
+
+  
+};
 
 
