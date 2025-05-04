3d03e3c858f6d9f78b37f806ab67ec423c5c4ffd
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-04 19:01:48 +0530

    Component Definition – layout This is a layout component that receives a children prop, which includes any page content rendered inside it (like /dashboard/forms, /dashboard/analytics, etc.).
    It returns a JSX layout with two main parts:

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,5 +2,9 @@ import DashboardSidebar from "@/components/Sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import React from "react";
 
-
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    
+  );
+};
 
