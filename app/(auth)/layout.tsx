a966705a200476a5c1db6ee8acc9eae3344cb9c9
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-04 14:28:20 +0530

    Component Props (children) children: This is a special prop in React used for passing content into a component. Anything placed inside the layout component when it's used will be passed as children. This is useful for creating reusable layouts.
    React.ReactNode: This type is a special type in React that represents anything that can be rendered. It can include:
    
    Strings
    
    Numbers
    
    JSX elements
    
    Arrays or fragments of JSX elements
    
    In essence, children allows the layout component to wrap and style any content passed to it.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const layout = ({children}:{children:React.ReactNode}) => {
   return (
     <div className='flex items-center justify-center h-screen w-screen'>
-      
+      {children}
     </div>
   )
 }
