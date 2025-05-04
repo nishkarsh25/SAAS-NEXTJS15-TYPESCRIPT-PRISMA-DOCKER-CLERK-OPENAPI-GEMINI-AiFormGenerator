f9303e9cf8b0b0e5597339f33f2e8cd8974e9682
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-04 19:06:44 +0530

    The layout Component This is a functional component that renders a layout structure with a sidebar and a content area. It accepts a children prop, which represents the nested content of the page that will be injected into the layout.

--- app/forms/[formId]/layout.tsx
+++ app/forms/[formId]/layout.tsx
@@ -2,5 +2,7 @@ import DashboardSidebar from "@/components/Sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import React from "react";
 
-
+const layout = ({ children }: { children: React.ReactNode }) => {
+ 
+};
 
