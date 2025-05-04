5a82bc34aa80eadf99afb3b9c9ee6682b3388ee7
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-04 19:04:49 +0530

    Component Definition – page The page component is an async server-side function that leverages Next.js' server-side rendering (SSR) capabilities.

--- app/dashboard/upgrade/page.tsx
+++ app/dashboard/upgrade/page.tsx
@@ -2,5 +2,8 @@ import PricingPage from '@/components/PricingPage'
 import { currentUser } from '@clerk/nextjs/server'
 import React from 'react'
 
-
+const page = async () => {
+  const user = await currentUser();
+  
+}
 
