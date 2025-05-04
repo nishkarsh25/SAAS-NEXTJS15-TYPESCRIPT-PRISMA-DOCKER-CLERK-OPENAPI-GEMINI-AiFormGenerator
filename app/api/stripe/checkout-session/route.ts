b7986d09bd515a9f6d0a83fc6b94b655317606b4
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-04 14:46:29 +0530

    Stripe Checkout Session Creation If all inputs are valid, the function proceeds to create a Checkout session via Stripe.
    stripe.checkout.sessions.create({...}) includes:
    payment_method_types: Specifies acceptable payment methods — here, only "card" is allowed.
    
    line_items: Describes what the user is paying for:
    
    price_data: Sets the currency, price (converted from dollars to cents), and product name.
    
    quantity: Always 1, meaning it’s a one-time charge.
    
    mode: Set to "payment", which means a one-time payment (not subscription).
    
    success_url / cancel_url: URLs to redirect the user based on payment success or cancellation.
    
    metadata: Additional key-value pairs sent with the payment, useful for identifying the user in Stripe's dashboard or for use in webhooks.
    
    Return the Stripe Session URL
    If the session is successfully created:
    
    Responds with a 200 OK status and returns:
    
    sessionId: Stripe's unique ID for the session
    
    url: The checkout link the frontend can redirect the user to for payment

--- app/api/stripe/checkout-session/route.ts
+++ app/api/stripe/checkout-session/route.ts
@@ -25,7 +25,44 @@ export async function POST(req: Request) {
     ? `${process.env.NEXT_PUBLIC_BASE_URL}`
     : null;
 
-  
+  if (!successUrl || !cancelUrl) {
+    return NextResponse.json(
+      {
+        error: "Frontend Url invalid",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
 
-  
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            unit_amount: price * 100,
+            product_data: {
+              name: `You are choosing ${plan} plan`,
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: successUrl,
+      cancel_url: cancelUrl,
+      metadata: {
+        userId: userId as string,
+      },
+    });
+    return NextResponse.json(
+      { sessionId: session.id, url: session.url },
+      { status: 200 }
+    );
+  } catch (error) {
+    
+  }
 }
