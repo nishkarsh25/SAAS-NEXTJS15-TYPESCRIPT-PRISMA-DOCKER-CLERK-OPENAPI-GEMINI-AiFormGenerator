87157ba651f3b00d0140cfe1a99abc1e31f55fb7
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-04 13:42:28 +0530

    Purpose of the Function The function's main goal is to:
    Validate the user.
    
    Ensure the form exists and belongs to the user.
    
    Update the form’s published status to true.

--- actions/publishForm.ts
+++ actions/publishForm.ts
@@ -3,4 +3,22 @@
 import prisma from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 
+export const publishForm = async (formId: number) => {
+  try {
+    
+    
 
+    
+
+    
+
+    
+
+    
+
+
+    
+  } catch (error) {
+    
+  }
+};
